f0d4b6bb6a65801922479c036f6eb25ce860231a
leiper.mike <leiper123.mike@outlook.com>
2025-01-16 16:21:01 -0500

    added to_html_node to textnode and tests

--- src/tests/test_textnode.py
+++ src/tests/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from src.textnode import TextNode, TextType
-
+from src.leafnode import LeafNode
 
 class TestTextNode(unittest.TestCase):
      def test_eq(self):
@@ -20,5 +20,36 @@ class TestTextNode(unittest.TestCase):
           node = TextNode("This is a text node", TextType.BOLD)
           node2 = TextNode("This is a text node", "bold")
           self.assertNotEqual(node, node2)
+     def test_to_html_node(self):
+          node = TextNode("This is a text node", TextType.BOLD)
+          node2 = TextNode("This is a text node", TextType.BOLD, url="https://www.google.com")
+          leaf1 = node.to_html_node()
+          leaf2 = node2.to_html_node()
+          self.assertEqual(leaf1,leaf2)
+          self.assertIsInstance(leaf1,LeafNode)
+          with self.assertRaises(TypeError) as e:
+               TextNode("test", "wrong").to_html_node()
+          self.assertEqual(str(e.exception), "text_type must be a valid member of the TextType enum")
+     def test_to_html_node_types(self):
+          leaf1 = TextNode("This is a text node", TextType.TEXT).to_html_node()
+          leaf2 = TextNode("This is a text node", TextType.BOLD).to_html_node()
+          leaf3 = TextNode("This is a text node", TextType.ITALIC).to_html_node()
+          leaf4 = TextNode("This is a text node", TextType.CODE).to_html_node()
+          leaf5 = TextNode("This is a text node", TextType.LINK, url="https://www.google.com").to_html_node()
+          leaf6 = TextNode("This is a text node", TextType.IMAGE, url="https://www.google.com").to_html_node()
+          self.assertEqual(leaf1.tag,"")
+          self.assertEqual(leaf2.tag,"b")
+          self.assertEqual(leaf3.tag,"i")
+          self.assertEqual(leaf4.tag,"code")
+          self.assertEqual(leaf5.tag,"a")
+          self.assertEqual(leaf5.props,{"href":"https://www.google.com"})
+          self.assertEqual(leaf6.props,{'alt': 'This is a text node', 'src': 'https://www.google.com'})
+     def test_to_html_node_no_url(self):
+          with self.assertRaises(ValueError) as e:
+               TextNode("test", TextType.LINK).to_html_node()
+          self.assertEqual(str(e.exception), "url must be provided for text nodes with link text type")
+          with self.assertRaises(ValueError) as ex:
+               TextNode("test", TextType.IMAGE).to_html_node()
+          self.assertEqual(str(ex.exception), "url must be provided for text nodes with img text type")      
 if __name__ == "__main__":
      unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,9 +1,10 @@
 from enum import Enum
 
+from src.leafnode import LeafNode
 class TextType(Enum):
-     NORMAL = "normal"
+     TEXT = "text"
      BOLD = "bold"
-     ITALICS = "italics"
+     ITALIC = "italic"
      CODE = "code"
      LINK = "link"
      IMAGE = "image"
@@ -16,3 +17,25 @@ class TextNode():
           return self.text == other.text and self.text_type == other.text_type and self.url == other.url
      def __repr__(self):
           return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+     def to_html_node(self):
+          if not self.text_type in TextType:
+               raise TypeError("text_type must be a valid member of the TextType enum")
+          match self.text_type:
+               case TextType.TEXT:
+                    return LeafNode("", self.text)
+               case TextType.BOLD:
+                    return LeafNode("b", self.text)
+               case TextType.ITALIC:
+                    return LeafNode("i", self.text)
+               case TextType.CODE:
+                    return LeafNode("code", self.text)
+               case TextType.LINK:
+                    if not self.url or not isinstance(self.url,str):
+                         raise ValueError("url must be provided for text nodes with link text type")
+                    return LeafNode("a", self.text, {"href":self.url})
+               case TextType.IMAGE:
+                    if not self.url or not isinstance(self.url,str):
+                         raise ValueError("url must be provided for text nodes with img text type")
+                    return LeafNode("img","",{"src":self.url,"alt":self.text})
+               case _:
+                    raise Exception("Could not match text_type to a member of TextType enum")
